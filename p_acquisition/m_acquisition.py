d732dd676c0694e6bfd5f6a48aacc452f14da556
ChristianJavierMelo <christian.javier.melo@gmail.com>
2020-06-30 10:19:54 +0200

    acquisition&wrangling checkpoint

--- p_acquisition/m_acquisition.py
+++ p_acquisition/m_acquisition.py
@@ -1,7 +1,20 @@
 import pandas as pd
+from sqlalchemy.pool import StaticPool
+from sqlalchemy import create_engine
 
-# acquisition functions
-
-def acquire():
-    data = pd.read_csv('./data/raw/vehicles.csv')
-    return data
+def getall(path):
+    print('connecting...')
+    engine = create_engine(f'sqlite:///{path}', poolclass=StaticPool)
+    header_tables = engine.table_names()
+    for table in range(0, len(header_tables)):
+        single = header_tables[table]
+        query = f'SELECT * FROM {single}'
+        data = pd.read_sql(query, con=engine)
+        if table == 0:
+            raw_data = data
+        else:
+            raw_data = pd.merge(raw_data, data, how='left', on='uuid')
+    print('receiving...')
+    raw_data.to_csv('data/raw/raw_data_compile.csv', index=False)
+    print('copy saved')
+    return raw_data
--- p_wrangling/m_wrangling.py
+++ p_wrangling/m_wrangling.py
@@ -1,7 +1,65 @@
 import pandas as pd
+import numpy as np
+import requests
+from bs4 import BeautifulSoup
 
-# wrangling functions
+def standardizeCountry(all):
+    print('starting with country standardize...')
+    url = 'https://ec.europa.eu/eurostat/statistics-explained/index.php/Glossary:Country_codes'
+    html = requests.get(url).content
+    soup = BeautifulSoup(html, 'html.parser')
+    frame = soup.find_all('td')
+    countries = [item.text.replace('\n', '').replace(' ', '') for item in frame]
+    countries_clean = list(filter(None, countries))
+    row_split = 2
+    rows_refactored = [countries_clean[x:x + row_split] for x in range(0, len(countries_clean), row_split)]
+    df = pd.DataFrame(rows_refactored, columns=['Country', 'country_code'])
+    df['country_code'] = df['country_code'].str.strip('()').astype(str)
+    all['country_code'] = all['country_code'].replace({'GR': 'EL',
+                                                       'GB': 'UK'})
+    allone = pd.merge(all, df, on='country_code')
+    print('receiving countries standardized...')
+    return allone
 
-def wrangle(df,year):
-    filtered = df[df['Year']==year]
-    return filtered
+def getjobtitle(allone):
+    print('starting with job standardize...')
+    jobs = allone['normalized_job_code'].unique()
+    job_dict = []
+    for id in jobs:
+        response = requests.get(f'http://api.dataatwork.org/v1/jobs/{id}')
+        job_dict.append(response.json())
+    api_df = pd.DataFrame(job_dict)
+    rename_api_df = api_df.rename(columns={'uuid': 'normalized_job_code',
+                                               'title': 'Job_Title'})
+    sort_df = rename_api_df.set_index('normalized_job_code')
+    allone = allone.set_index('normalized_job_code')
+    alltwo = sort_df.join(allone, how='outer')
+    print('receiving jobs standardized...')
+    return alltwo
+
+def calculateAge(birthDate):
+    today = pd.Timestamp('2016').year
+    each_age = today-int(birthDate)
+    return each_age
+
+def realAge(alltwo):
+    print('starting wit age standardize...')
+    alltwo['age'] = alltwo['age'].str.replace('\D', '')
+    age = []
+    for birth in alltwo['age']:
+        if len(birth) == 4:
+            age.append(calculateAge(birth))
+        else:
+            age.append(birth)
+    alltwo['age'] = np.array(age)
+    print('receiving age standardized...')
+    return alltwo
+
+def test(alltwo):
+    data = alltwo[[]]
+
+def wrangling(all):
+    standardizedC = standardizeCountry(all)
+    standardizedJ = getjobtitle(standardizedC)
+    standardizedA = realAge(standardizedJ)
+    return standardizedA
